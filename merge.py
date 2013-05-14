972b8a258bca55bd1b2e3e2b942144f8fd1136a0
Clint Byrum <clint@fewbar.com>
2013-05-14 08:14:46 -0700

    Resolves some changed refs

--- merge.py
+++ merge.py
@@ -6,6 +6,7 @@ templates = list(sys.argv[1:])
 errors = []
 end_template={'HeatTemplateFormatVersion': '2012-12-12',
               'Description': []}
+resource_changes=[]
 for template_path in templates:
     template = yaml.safe_load(open(template_path))
     end_template['Description'].append(template.get('Description',
@@ -38,6 +39,8 @@ for template_path in templates:
             # XXX Assuming ImageId is always a Ref
             del end_template['Parameters'][rbody['Properties']['ImageId']['Ref']]
             role = rbody.get('Metadata', {}).get('OpenStack::Role', r)
+            if role != r:
+                resource_changes.append((r, role))
             if role in end_template.get('Resources', {}):
                 new_metadata = rbody.get('Metadata', {})
                 for m, mbody in iter(new_metadata.items()):
@@ -67,6 +70,26 @@ for template_path in templates:
                 end_template['Resources'] = {}
             end_template['Resources'][r] = rbody
 
+def fix_ref(item, old, new):
+    if isinstance(item, dict):
+        copy_item = dict(item)
+        for k, v in iter(copy_item.items()):
+            if k == 'Ref' and v == old:
+                item[k] = new
+                continue
+            if k == 'Fn::GetAtt' and isinstance(v, list) and v[0] == old:
+                new_list = list(v)
+                new_list[0] = new
+                item[k] = new_list
+                continue
+            fix_ref(v, old, new)
+    elif isinstance(item, list):
+        copy_item = list(item)
+        for v in item:
+            fix_ref(v, old, new)
+
+for change in resource_changes:
+    fix_ref(end_template, change[0], change[1])
             
 if errors:
     for e in errors:
